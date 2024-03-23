baff841b74a4aea4b8d5eed87e8bb498edc94a17
Carmine Scarpitta <cscarpit@cisco.com>
2024-03-23 17:25:39 +0100

    zebra: Alloc/Release SIDs to daemons upon request
    Previous commits introduced two new ZAPI operations,
    `ZEBRA_SRV6_MANAGER_GET_SRV6_SID` and
    `ZEBRA_SRV6_MANAGER_RELEASE_SRV6_SID`. These operations allow a daemon
    to interact with the SRv6 SID Manager to get and release an SRv6 SID,
    respectively.
    
    This commit extends the SID Manager by adding logic to process the
    requests `ZEBRA_SRV6_MANAGER_GET_SRV6_SID` and
    `ZEBRA_SRV6_MANAGER_RELEASE_SRV6_SID`, and allocate/release SIDs to
    requesting daemons.
    
    Signed-off-by: Carmine Scarpitta <cscarpit@cisco.com>

--- zebra/zebra_srv6.h
+++ zebra/zebra_srv6.h
@@ -288,6 +288,13 @@ DECLARE_HOOK(srv6_manager_release_chunk,
 	      vrf_id_t vrf_id),
 	     (client, locator_name, vrf_id));
 
+DECLARE_HOOK(srv6_manager_get_sid,
+	     (struct zebra_srv6_sid **sid, struct zserv *client,
+	      struct srv6_sid_ctx *ctx, struct in6_addr *sid_value,
+	      const char *locator_name),
+	     (sid, client, ctx, sid_value, locator_name));
+DECLARE_HOOK(srv6_manager_release_sid,
+	     (struct zserv *client, struct srv6_sid_ctx *ctx), (client, ctx));
 DECLARE_HOOK(srv6_manager_get_locator,
 	     (struct srv6_locator **locator, struct zserv *client,
 	      const char *locator_name),
@@ -351,6 +358,14 @@ zebra_srv6_sid_alloc(struct zebra_srv6_sid_ctx *ctx, struct in6_addr *sid_value,
 extern void zebra_srv6_sid_free(struct zebra_srv6_sid *sid);
 extern void delete_zebra_srv6_sid(void *val);
 
+extern void srv6_manager_get_sid_call(struct zebra_srv6_sid **sid,
+				      struct zserv *client,
+				      struct srv6_sid_ctx *ctx,
+				      struct in6_addr *sid_value,
+				      const char *locator_name);
+extern void srv6_manager_release_sid_call(struct zserv *client,
+					  struct srv6_sid_ctx *ctx);
+
 extern void srv6_manager_get_locator_call(struct srv6_locator **locator,
 					  struct zserv *client,
 					  const char *locator_name);
@@ -359,6 +374,9 @@ extern int get_srv6_sid(struct zebra_srv6_sid **sid, struct srv6_sid_ctx *ctx,
 			struct in6_addr *sid_value, const char *locator_name);
 extern int release_srv6_sid(struct zserv *client,
 			    struct zebra_srv6_sid_ctx *zctx);
+extern int release_daemon_srv6_sids(struct zserv *client);
+extern int srv6_manager_get_sid_response(struct zebra_srv6_sid *sid,
+					 struct zserv *client);
 
 extern struct zebra_srv6_sid_ctx *zebra_srv6_sid_ctx_alloc(void);
 extern void zebra_srv6_sid_ctx_free(struct zebra_srv6_sid_ctx *ctx);
