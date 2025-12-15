9a2b7b7e6227366a891b8c47b9235110879e42be
Donald Sharp <sharpd@nvidia.com>
2025-12-15 13:28:25 -0500

    bgpd: peer_create now accepts the connection direction
    Modify peer_create to take a connection direction and to
    pass it on appropriately.
    
    Signed-off-by: Donald Sharp <sharpd@nvidia.com>

--- bgpd/bgp_network.c
+++ bgpd/bgp_network.c
@@ -515,7 +515,6 @@ static void bgp_accept(struct event *event)
 			incoming = dynamic_peer->connection;
 			/* Dynamic neighbor has been created, let it proceed */
 			incoming->fd = bgp_sock;
-			incoming->dir = CONNECTION_INCOMING;
 
 			incoming->su_local = sockunion_getsockname(incoming->fd);
 			incoming->su_remote = sockunion_dup(&su);
@@ -646,8 +645,8 @@ static void bgp_accept(struct event *event)
 		peer_delete(peer->doppelganger);
 	}
 
-	doppelganger = peer_create(&su, peer->conf_if, bgp, peer->local_as, peer->as, peer->as_type,
-				   NULL, false, NULL);
+	doppelganger = peer_create(&su, peer->conf_if, bgp, peer->local_as, peer->as,
+				   peer->as_type, NULL, false, NULL, CONNECTION_INCOMING);
 
 	incoming = doppelganger->connection;
 
@@ -668,7 +667,6 @@ static void bgp_accept(struct event *event)
 	peer->doppelganger = doppelganger;
 
 	incoming->fd = bgp_sock;
-	incoming->dir = CONNECTION_INCOMING;
 	incoming->su_local = sockunion_getsockname(incoming->fd);
 	incoming->su_remote = sockunion_dup(&su);
 
